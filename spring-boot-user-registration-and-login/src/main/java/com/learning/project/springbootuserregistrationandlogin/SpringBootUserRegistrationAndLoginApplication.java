e65870101aa77a43d18476e2f980a422a5fcf689
Tatiana Samsonova <tatiana.samsonova228@gmail.com>
2020-07-22 08:37:36 +0300

    init commit

---
+++ spring-boot-user-registration-and-login/src/main/java/com/learning/project/springbootuserregistrationandlogin/SpringBootUserRegistrationAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.learning.project.springbootuserregistrationandlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUserRegistrationAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootUserRegistrationAndLoginApplication.class, args);
+	}
+
+}
---
+++ spring-boot-user-registration-and-login/src/test/java/com/learning/project/springbootuserregistrationandlogin/SpringBootUserRegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.project.springbootuserregistrationandlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUserRegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
