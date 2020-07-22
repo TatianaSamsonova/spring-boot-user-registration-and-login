1dcee0e4ffd1c9d90204c5ad2b8a5c56c2325b14
Tatiana Samsonova <tatiana.samsonova228@gmail.com>
2020-07-22 09:15:48 +0300

    Create User Entity

---
+++ spring-boot-user-registration-and-login/src/main/java/com/learning/project/springbootuserregistrationandlogin/entity/User.java
@@ -0,0 +1,83 @@
+package com.learning.project.springbootuserregistrationandlogin.entity;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author TatianaSamsonova
+ * @created 22/07/2020 - 9:02 AM
+ * @project spring-boot-user-registration-and-login
+ */
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+    @Builder.Default
+    private UserRole userRole = UserRole.USER;
+
+    @Builder.Default
+    private Boolean locked = false;
+
+    @Builder.Default
+    private Boolean enabled = false;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        final SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(userRole.name());
+        return Collections.singletonList(simpleGrantedAuthority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; //userName is an  email here
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true; //do not use expired field -> hardcode true
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
---
+++ spring-boot-user-registration-and-login/src/main/java/com/learning/project/springbootuserregistrationandlogin/entity/UserRole.java
@@ -0,0 +1,11 @@
+package com.learning.project.springbootuserregistrationandlogin.entity;
+
+/**
+ * @author TatianaSamsonova
+ * @created 22/07/2020 - 8:58 AM
+ * @project spring-boot-user-registration-and-login
+ */
+public enum UserRole {
+    ADMIN,
+    USER
+}
